d7c2cb3162d6fcb4ea8a8495b083e77ce0d7b772
Drakvlaa <Filip.sobczuk@o2.pl>
2024-03-26 00:22:32 +0100

    Improved code clarity

--- main.cpp
+++ main.cpp
@@ -3,23 +3,24 @@
 #include <stdio.h>
 #include <windows.h>
 #include <sys/types.h>
-#include <dirent.h>
 #include <filesystem>
 #include <string.h>
 #include <sstream>
 #include "colors.hpp"
-const char *add_char(const char *a, const char *b)
+#include <vector>
+std::vector<std::string> commands;
+
+void OpenCommandPrompt();
+
+const char *add_char(const std::string &a, const std::string &b)
 {
-  char buffer[256];
-  strncpy(buffer, a, sizeof(buffer));
-  strncat(buffer, b, sizeof(buffer));
-  const char *c = buffer;
-  return c;
+  return (a + b).c_str();
 }
+
 const char *GetUserVariablePath()
 {
   static const char path[] = "Path";
-  static BYTE buffer1[1000000];
+  static BYTE buffer1[1];
   DWORD buffsz1 = sizeof(buffer1);
   {
     const char key_name[] = "Environment";
@@ -33,6 +34,7 @@ const char *GetUserVariablePath()
     return nullptr;
   }
 }
+
 void SetUserVariablePath(const char *name)
 {
   HKEY hkey;
@@ -40,32 +42,44 @@ void SetUserVariablePath(const char *name)
   const char key_name[] = "Environment";
   const char *path = add_char(GetUserVariablePath(), name);
   regOpenResult = RegOpenKeyEx(HKEY_CURRENT_USER, key_name, 0, KEY_ALL_ACCESS, &hkey);
-  LPCSTR stuff = "Path3";
+  LPCSTR stuff = "Path2";
   RegSetValueEx(hkey, stuff, 0, REG_SZ, (BYTE *)path, strlen(path));
   RegCloseKey(hkey);
 }
-void SaveFile(std::string filename, FILE* file)
+
+void SaveFile(std::string filename, FILE *file)
 {
   std::filesystem::path p = filename;
   SetUserVariablePath(filename.c_str());
   fclose(file);
   std::cout << "File saved!" << std::endl;
-  std::string path = std::filesystem::absolute(p).string();
-  exit(0);
+  OpenCommandPrompt();
+  commands.push_back(filename);
 }
+
+void DisplayFiles(std::vector<std::string> list)
+{
+  std::cout << BLUE "Your commands: " RESET << std::endl;
+  for (std::string com : list)
+  {
+    int i = 1;
+    std::cout << YELLOW << i << "." << com << RESET << std::endl;
+    i++;
+  }
+  OpenCommandPrompt();
+}
+
 void createCommand()
 {
-  std::cout << "Enter file name: " << std::endl;
+  std::cout << GREEN "Enter file name: " RESET << std::endl;
   std::string filename;
   getline(std::cin, filename);
   FILE *file = fopen((filename + ".bat").c_str(), "w");
-  std::cout << file << std::endl;
   std::filesystem::path p = filename;
-  // std::cout << std::filesystem::absolute(p) << std::endl;
 
   if (!file)
   {
-    fprintf(stderr, "Error!");
+    fprintf(stderr, RED "Error!" RESET);
     return;
   }
   std::string line;
@@ -91,29 +105,29 @@ void createCommand()
   fclose(file);
 }
 
-int main()
+void OpenCommandPrompt()
 {
-  DIR *dir = opendir(".");
-  std::cout << dir << std::endl;
   std::cout << "What would you like to do?\n"
                "1. Create new command\n"
                "2. See all created commands\n"
-               "3. help\n"
-               "4. type \".exit\" to leave\n";
+               "3. type \".exit\" or \"3\" to leave\n";
 
   while (true)
   {
     std::string input;
     getline(std::cin, input);
-    if (input == ".exit")
-      break;
     if (input == "1")
       createCommand();
     if (input == "2")
-      break; // printCommands();
-    if (input == "3")
-      break; // help();
+      DisplayFiles(commands);
+    if (input == ".exit" || "3")
+      exit(0);
   }
+  return;
+}
 
+int main()
+{
+  OpenCommandPrompt();
   return 0;
 }
