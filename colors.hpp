8d4052095877c5078258c5b19ec6a84ee8bee795
Drakvlaa <Filip.sobczuk@o2.pl>
2024-03-25 06:40:36 +0100

    added more text colors & visual changes

--- colors.hpp
+++ colors.hpp
@@ -1,8 +1,23 @@
-#include <iostream>
-#define PURPLE "\033[107m"
-#define  RESET "\033[0m"
+#define BLACK "\033[30m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[1;33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
+#define LIGHT_GRAY "\033[37m"
+#define DARK_GRAY "\033[90m"
+#define LIGHT_RED "\033[91m"
+#define LIGHT_GREEN "\033[92m"
+#define LIGHT_YELLOW "\033[93m"
+#define LIGHT_BLUE "\033[94m"
+#define LIGHT_MAGENTA "\033[95m"
+#define LIGHT_CYAN "\033[96m"
+#define WHITE "\033[97 m"
+#define BOLD "\033[1m"
+#define UNDERLINE "\033[4m"
+#define NO_UNDERLINE "\033[24m"
+#define NEGATIVE "\033[7m"
+#define POSITIVE "\033[27m"
+#define RESET "\033[0m"
 
-int main(int argc, char const *argv[])
-{
-    return 0;
-}
--- main.cpp
+++ main.cpp
@@ -6,33 +6,52 @@
 #include <dirent.h>
 #include <filesystem>
 #include <string.h>
-void SetUserVariablePath()
+#include <sstream>
+#include "colors.hpp"
+const char *add_char(const char *a, const char *b)
 {
-  HKEY hkey;
-  long regOpenResult;
-  const char key_name[] = "Environment";
-  const char path[] = "D:/custom_command"; // new_value path need to update
-  regOpenResult = RegOpenKeyEx(HKEY_CURRENT_USER, key_name, 0, KEY_ALL_ACCESS, &hkey);
-  LPCSTR stuff = "Path2"; // Variable Name
-  RegSetValueEx(hkey, stuff, 0, REG_SZ, (BYTE *)path, strlen(path));
-  RegCloseKey(hkey);
+  char buffer[256];
+  strncpy(buffer, a, sizeof(buffer));
+  strncat(buffer, b, sizeof(buffer));
+  const char *c = buffer;
+  return c;
 }
-void GetUserVariablePath()
+const char *GetUserVariablePath()
 {
-  static const char path[] = "Path"; // Variable Name
+  static const char path[] = "Path";
   static BYTE buffer1[1000000];
   DWORD buffsz1 = sizeof(buffer1);
   {
-    // HKEY_CURRENT_USER\Environment
     const char key_name[] = "Environment";
     HKEY key;
 
     if (RegOpenKeyExA(HKEY_CURRENT_USER, key_name, 0, KEY_QUERY_VALUE, std::addressof(key)) == 0 &&
         RegQueryValueExA(key, path, nullptr, nullptr, buffer1, std::addressof(buffsz1)) == 0)
     {
-      std::cout << "The updated value of the user variable is :  " << reinterpret_cast<const char *>(buffer1) << '\n';
+      return reinterpret_cast<const char *>(buffer1);
     }
+    return nullptr;
   }
+}
+void SetUserVariablePath(const char *name)
+{
+  HKEY hkey;
+  long regOpenResult;
+  const char key_name[] = "Environment";
+  const char *path = add_char(GetUserVariablePath(), name);
+  regOpenResult = RegOpenKeyEx(HKEY_CURRENT_USER, key_name, 0, KEY_ALL_ACCESS, &hkey);
+  LPCSTR stuff = "Path3";
+  RegSetValueEx(hkey, stuff, 0, REG_SZ, (BYTE *)path, strlen(path));
+  RegCloseKey(hkey);
+}
+void SaveFile(std::string filename, FILE* file)
+{
+  std::filesystem::path p = filename;
+  SetUserVariablePath(filename.c_str());
+  fclose(file);
+  std::cout << "File saved!" << std::endl;
+  std::string path = std::filesystem::absolute(p).string();
+  exit(0);
 }
 void createCommand()
 {
@@ -40,8 +59,9 @@ void createCommand()
   std::string filename;
   getline(std::cin, filename);
   FILE *file = fopen((filename + ".bat").c_str(), "w");
+  std::cout << file << std::endl;
   std::filesystem::path p = filename;
-  std::cout << std::filesystem::absolute(p) << std::endl;
+  // std::cout << std::filesystem::absolute(p) << std::endl;
 
   if (!file)
   {
@@ -49,20 +69,13 @@ void createCommand()
     return;
   }
   std::string line;
-  std::cout << "\033[35mFile editor mode\033[0m" << std::endl;
+  std::cout << MAGENTA "File editor mode" RESET << std::endl;
   do
   {
     std::cout << ": ";
     getline(std::cin, line);
     if (line == ".save")
-    {
-      fclose(file);
-      std::cout << "File saved!" << std::endl;
-      std::string path = std::filesystem::absolute(p).string() + ".bat\"";
-      SetUserVariablePath();
-      exit(0);
-      // savefile();
-    }
+      SaveFile(filename, file);
     line += '\n';
     for (char c : line)
     {
@@ -80,7 +93,6 @@ void createCommand()
 
 int main()
 {
-  GetUserVariablePath();
   DIR *dir = opendir(".");
   std::cout << dir << std::endl;
   std::cout << "What would you like to do?\n"
